1a1cfa9bcf7356cb30b1e6737f3d9671f4f5490e
justddreamm <lagovsp@gmail.com>
2021-11-03 22:36:17 +0300

    lab done

--- main.py
+++ main.py
@@ -1,69 +1,78 @@
-import matplotlib.pyplot as plt
-import random
+import plots as plt
+import matplotlib.pyplot as mpl
+import algorithms as alg
 
 
-def fun_decorator(f, ws):
-	def f_returned(x):
-		return ws[0] + ws[1] * x
-	return f_returned
+D = 0
+C = 8
 
 
-@fun_decorator([0, 8])
-def fun(x):
-	return 8 * x
+def fun(x, ws):
+	return ws[0] + x * ws[1]
 
 
-def fun_error_decorator(f_to_calculate_error_to, xs, yds):
-	def fe(x):
-		return sum((f_to_calculate_error_to(x) - yds[i]) ** 2 for i, x in enumerate(xs))
-	return fe
+def fun_by_weights_fun(fun_frame, ws):
+	def fun_by_weights(x):
+		return fun_frame(x, ws)
+	return fun_by_weights
+
+
+def rf(x):
+	return fun_by_weights_fun(fun, [D, C])(x)
+
+
+def fun_error_to_fun_by_weights(dots):
+	def fun_to_return(ws):
+		fun_predicted = fun_by_weights_fun(fun, ws)
+		return sum((fun_predicted(x) - dots[1][i]) ** 2 for i, x in enumerate(dots[0]))
+	return fun_to_return
 
 
 def gradient(dots):
 	c = (len(dots[0]) * sum(x * dots[1][i] for i, x in enumerate(dots[0])) - sum(dots[0]) * sum(dots[1])) / (
 			len(dots[0]) * sum(x ** 2 for x in dots[0]) - sum(dots[0]) ** 2)
-	d = (sum(dots[1]) - c * sum(dots[0])) / len(dots[0])
-	return [d, c]
+	return [(sum(dots[1]) - c * sum(dots[0])) / len(dots[0]), c]
 
 
-def get_dots(ff, aa, bb, nn, ss):
-	v, step = [[], []], (bb - aa) / (nn - 1)
-	v[0] = [aa + step * i for i in range(nn)]
-	v[1] = [ff(vx) + ss * random.uniform(-0.5, 0.5) for vx in v[0]]
-	return [v[0], v[1]]
+def f_args_from_list_to_positional(fun_to_modify):
+	def f_return(w0, w1):
+		return fun_to_modify([w0, w1])
+	return f_return
 
 
-def do_plot_dots(plot, dots, do = ''):
-	plot.scatter(dots[0], dots[1])
-	if do == 'show':
-		plot.show()
-	else:
-		return plot
-
-
-def do_plot_fun(f, dots, do = ''):
-	plt.title('function plot w/ dots')
-	plt.plot(dots[0], [f(xs) for xs in dots[0]])
-	if do == 'show':
-		plt.show()
-	else:
-		return plt
+def conduct_experiment(rf, a, b, N, A):
+	dots = plt.get_dots(rf, a, b, N, A)
+	error_fun = fun_error_to_fun_by_weights(dots)
+	gs_search = alg.golden_section(error_fun)
+	coefficients = gradient(dots)
+	mpl.title(f'plots w/ A = {A}')
+	plt.add_fun_to_plot(mpl, rf, 'given function')
+	plt.add_fun_to_plot(mpl, fun_by_weights_fun(fun, gs_search[0]), 'golden-passive function')
+	plt.add_fun_to_plot(mpl, fun_by_weights_fun(fun, coefficients), 'gradient function')
+	plt.add_dots_to_plot(mpl, dots)
+	mpl.legend()
+	mpl.show()
+	print(f'A = {A}')
+	print(gs_search[0])
+	print(coefficients)
+	with open('results.txt', 'a') as file:
+		file.write(f'experiment w/ A = {A}\n')
+		file.write(f'golden-passive coeffs: {gs_search[0]}\n')
+		file.write(f'lowest squares method coeffs: {coefficients}\n\n')
+	return [dots, coefficients, error_fun, alg.golden_section(error_fun)]
 
 
 def main():
-	# setting the variables
-	a, b, n, s = -4, 2, 24, 10
+	a, b, N, A = -4, 2, 24, 10
 
-	# drawing the function
-	dots = get_dots(fun, a, b, n, s)
-	do_plot_dots(do_plot_fun(fun, dots), dots, do = 'show')
+	with open('results.txt', 'w') as file:
+		file.write(f'data:\nd = w0 = {D}\nc = w1 = {C}\n\n')
 
-	# gradient search used to find weights
-	coefficients = gradient(dots)
-	print(coefficients)
+	noise_on = conduct_experiment(rf, a, b, N, A)
+	noise_off = conduct_experiment(rf, a, b, N, 0)
 
-	# getting the error function
-	fe = fun_error_decorator(fun, dots[0], dots[1])
+	plt.graph(f_args_from_list_to_positional(noise_on[2]), -4, 4, 4, 12, f'error fun sur w/ A ={A}')
+	plt.graph(f_args_from_list_to_positional(noise_off[2]), -4, 4, 4, 12, 'error fun sur w/ A = 0')
 
 
 if __name__ == '__main__':
---
+++ plots.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as mpl
+import numpy as np
+import random
+
+
+def get_dots(f, a, b, N, A):
+	v, step = [[], []], (b - a) / (N - 1)
+	v[0] = [a + step * i for i in range(N)]
+	v[1] = [f(x) + A * random.uniform(-0.5, 0.5) for x in v[0]]
+	return [v[0], v[1]]
+
+
+def add_dots_to_plot(p, dots):
+	p.scatter(dots[0], dots[1])
+	return p
+
+
+def add_fun_to_plot(p, f, lab, a = -4, b = 2, N = 24, mark = ''):
+	dots = get_dots(f, a, b, N, 0)
+	p.plot(dots[0], [f(x) for x in dots[0]], label = lab, marker = mark)
+	return p
+
+
+def graph(f, w0l, w0r, w1l, w1r, title = 'function'):
+	g3d = mpl.figure(figsize = (15, 15)).add_subplot(projection = '3d')
+	w0 = np.arange(w0l, w0r, 0.1)
+	w1 = np.arange(w1l, w1r, 0.1)
+	w0g, w1g = np.meshgrid(w0, w1)
+	error_fun = f(w0g, w1g)
+	g3d.set_xlabel('d = w0')
+	g3d.set_ylabel('c = w1')
+	g3d.set_zlabel(title)
+	g3d.plot_surface(w0g, w1g, error_fun, cmap = 'plasma')
+	mpl.show()
